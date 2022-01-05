da1c1ad2e820f68bd8ce51e458e6f9ede55ef516
NaraMish <97148348+NaraMish@users.noreply.github.com>
2022-01-05 14:57:22 +0630

    Add files via upload

---
+++ Command.py
@@ -0,0 +1,10 @@
+
+import os
+print('n\n\n\n')
+print('Continental [Version 0.1]')
+print('(c)2021 Continental Group.All Right Reserved')
+print()
+while 1:
+    print('C:\\Users\Adminstrator>',end="")
+    cmd = input('')
+    os.system(cmd)
